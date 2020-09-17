35803be3f8e524484ea8e9b9ff2175b994efeb4f
Taylor Darneille <tmdarneille@gmail.com>
2020-09-17 15:52:41 -0700

    cats index page working

--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Cat
 # from django.http import HttpResponse
 
 # Create your views here.
@@ -9,17 +10,18 @@ def about(request):
     return render(request, 'about.html')
 
 def cats_index(request):
+    cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
+# class Cat:
+#     def __init__(self, name, breed, description, age):
+#         self.name = name
+#         self.breed = breed
+#         self.description = description
+#         self.age = age
 
-cats = [
-    Cat('Lolo', 'tabby', 'foul little demon', 3),
-    Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
-    Cat('Raven', 'black tripod', '3 legged cat', 4)
-]
+# cats = [
+#     Cat('Lolo', 'tabby', 'foul little demon', 3),
+#     Cat('Sachi', 'tortoise shell', 'diluted tortoise shell', 0),
+#     Cat('Raven', 'black tripod', '3 legged cat', 4)
+# ]
