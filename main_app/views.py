267b3c2f47aaf0a151c9a67595acc9253c2f6bc0
Taylor Darneille <tmdarneille@gmail.com>
2020-09-17 12:26:34 -0700

    added about template and served it in the about view

--- main_app/views.py
+++ main_app/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Hello, World!</h1>')
 
 def about(request):
-    return HttpResponse('<h1>I am Taylor the Developer</h1>')
+    return render(request, 'about.html')
