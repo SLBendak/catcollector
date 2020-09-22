025afb6d228d78b6e27284e2f5e2380d4d56d368
Taylor Darneille <tmdarneille@gmail.com>
2020-09-22 08:24:45 -0700

    finished auth

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('cats/', views.cats_index, name='cats_index'),
-    path('cats/<int:cat_id>', views.cats_show, name='cats_show'),
+    path('cats/<int:cat_id>/', views.cats_show, name='cats_show'),
     path('cats/create/', views.CatCreate.as_view(), name='cats_create'),
     path('cats/<int:pk>/update/', views.CatUpdate.as_view(), name='cats_update'),
     path('cats/<int:pk>/delete/', views.CatDelete.as_view(), name='cats_delete'),
@@ -15,4 +15,9 @@ urlpatterns = [
     path('cattoys/create/', views.CatToyCreate.as_view(), name='cattoys_create'),
     path('cattoys/<int:pk>/update/', views.CatToyUpdate.as_view(), name='cattoys_update'),
     path('cattoys/<int:pk>/delete/', views.CatToyDelete.as_view(), name='cattoys_delete'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('signup/', views.signup_view, name='signup'),
+    # path('accounts/', include('django.contrib.auth.urls')),
+    # path('accounts/login', views.login, name='login'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,38 +1,81 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Cat, CatToy
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
-# Create your views here.
+########### USER ##############
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            print('HEY', user.username)
+            return HttpResponseRedirect('/user/'+str(user))
+        else:
+            HttpResponse('<h1>Try Again</h1>')
+    else:
+        form = UserCreationForm()
+        return render(request, 'signup.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect('/cats')
+
+def login_view(request):
+     # if post, then authenticate (user submitted username and password)
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        # form = LoginForm(request.POST)
+        if form.is_valid():
+            u = form.cleaned_data['username']
+            p = form.cleaned_data['password']
+            user = authenticate(username = u, password = p)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return HttpResponseRedirect('/user/'+u)
+                else:
+                    print('The account has been disabled.')
+            else:
+                print('The username and/or password is incorrect.')
+    else: # it was a get request so send the emtpy login form
+        # form = LoginForm()
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
 def index(request):
     return render(request, 'index.html')
 
 def about(request):
     return render(request, 'about.html')
 
+@login_required
 def profile(request, username):
     user = User.objects.get(username=username)
     cats = Cat.objects.filter(user=user)
     return render(request, 'profile.html', {'username': username, 'cats': cats})
 
+######## CATS ############
+
 def cats_index(request):
     cats = Cat.objects.all()
     return render(request, 'cats/index.html', {'cats': cats})
-
 
 def cats_show(request, cat_id):
     cat = Cat.objects.get(id=cat_id)
     return render(request, 'cats/show.html', {'cat': cat})
 
-class CatToyCreate(CreateView):
-    model = CatToy
-    fields = '__all__'
-    success_url = '/cattoys'
-
+@method_decorator(login_required, name='dispatch')
 class CatCreate(CreateView):
     model = Cat
-    fields = ['name', 'breed', 'description', 'age', 'cattoys']
+    fields = ['name', 'breed', 'description', 'age']
     success_url = '/cats'
 
     def form_valid(self, form):
@@ -42,6 +85,7 @@ class CatCreate(CreateView):
         print('PRINTING SELF.OBJECT', self.object.cattoys)
         return HttpResponseRedirect('/cats')
 
+@method_decorator(login_required, name='dispatch')
 class CatUpdate(UpdateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age', 'cattoys']
@@ -51,9 +95,12 @@ class CatUpdate(UpdateView):
         self.object.save()
         return HttpResponseRedirect('/cats/'+str(self.object.pk))
 
+@method_decorator(login_required, name='dispatch')
 class CatDelete(DeleteView):
     model = Cat
     success_url = '/cats'
+
+######### CATTOYS ##############
 
 def cattoys_index(request):
     cattoys = CatToy.objects.all()
@@ -63,11 +110,19 @@ def cattoys_show(request, cattoy_id):
     cattoy = CatToy.objects.get(id=cattoy_id)
     return render(request, 'cattoys/show.html', {'cattoy': cattoy})
 
+@method_decorator(login_required, name='dispatch')
+class CatToyCreate(CreateView):
+    model = CatToy
+    fields = '__all__'
+    success_url = '/cattoys'
+
+@method_decorator(login_required, name='dispatch')
 class CatToyUpdate(UpdateView):
     model = CatToy
     fields = ['name', 'color']
     success_url = '/cattoys'
 
+@method_decorator(login_required, name='dispatch')
 class CatToyDelete(DeleteView):
     model = CatToy
     success_url = '/cattoys'
